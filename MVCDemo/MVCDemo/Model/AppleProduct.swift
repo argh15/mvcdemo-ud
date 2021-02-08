6161bcf1c1a13b48f73674b69e7e4a016e57b312
Arghadeep <archakraborty@teksystems.com>
2021-02-08 21:18:43 +0530

    Grouping Model in place.

---
+++ MVCDemo/MVCDemo/Model/AppleProduct.swift
@@ -0,0 +1,21 @@
+//
+//  AppleProduct.swift
+//  MVCDemo
+//
+//  Created by Arghadeep  on 08/02/21.
+//
+
+import Foundation
+
+class AppleProduct {
+    
+    var name: String
+    var color: String
+    var price: Double
+    
+    init(name: String, color: String, price: Double) {
+        self.name = name
+        self.color = color
+        self.price = price
+    }
+}
